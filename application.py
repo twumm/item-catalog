ee224d590da7d374324cb65cd680e6ad6151ed1b
vagrant <vagrant@vagrant.vm>
2017-10-12 06:08:47 +0000

    Modify print syntax, add python 2 requirement

--- application.py
+++ application.py
@@ -108,7 +108,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(json.dumps("Token's client ID does not '\
                                 'match app's"), 401)
-        print("Token's client ID does not match app's.")
+        print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
     # Check if a user is already logged in
@@ -160,7 +160,7 @@ def gdisconnect():
     # Only disconnect a connected user
     access_token = login_session.get('access_token')
     if access_token is None:
-        print('Access Token is None')
+        print 'Access Token is None'
         response = make_response(json.dumps('Current user not '
                                             'connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
