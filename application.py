7bf2fb793eb329502e320518ee4296eb8ba9a2f5
twumm <martint.mensah@gmail.com>
2017-11-03 10:53:40 +0000

    Add secret key

--- application.py
+++ application.py
@@ -432,7 +432,7 @@ def create_user(user):
 
 
 # Run application on localhost
-if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+app.secret_key = 'super_secret_key'
+app.debug = True
+    # app.run(host='0.0.0.0', port=5000)
